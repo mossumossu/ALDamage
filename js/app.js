06b85f47da1fd7b5d9615bc7b421f8d78e2b63b5
mossumossu <mosstanks@gmail.com>
2019-01-17 14:03:57 -0500

    testing json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+let dropdown = $('#dd-weapon');
+
+dropdown.empty();
+
+dropdown.append('<option selected="true" disabled>Choose Weapon</option>');
+dropdown.prop('selectedIndex', 0);
+
+const url = 'https://mossumossu.github.io/ALDamage/res/weapons.json';
+
+// Populate dropdown with list of weapons
+$.getJSON(url, function (data) {
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.name).text(entry.name));
+  })
+});
