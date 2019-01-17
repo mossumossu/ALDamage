d4125f471304e498d03aa1196fa2720c06c49737
mossumossu <mosstanks@gmail.com>
2019-01-17 17:33:24 -0500

    added meta DD and CA guns, reduced number of ajax calls, fixed async

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
 let dropdown = $('#dd-weapon');
-
 dropdown.empty();
-
 dropdown.append('<option selected="true" disabled>Choose Weapon</option>');
 dropdown.prop('selectedIndex', 0);
 
 const url = 'https://mossumossu.github.io/ALDamage/res/weapons.json';
 
-// Populate dropdown with list of weapons
-$.getJSON(url, function (data) {
-  $.each(data, function (key, entry) {
-    dropdown.append($('<option></option>').attr('value', entry.Name).text(entry.Name));
-  })
+var weaponData = [];
+
+// populate weapon data array from .json
+function getWeaponData(){
+    return $.getJSON(url, function(data){
+        weaponData = data;
+    });
+};
+
+// populate weapon dropdown from array
+getWeaponData().done(function(){
+    weaponData.forEach(function(item, index) {
+        dropdown.append(new Option(item.Name, index));
+    });
 });
+
+getWeaponData();
 
 $(".dd-weapon").change(function () {
     var currentWeapon = $('#dd-weapon').val();
     var currentLevel = $('#dd-weaponLevel').val();
     var cRoF, cDPShot;
 
-    $.getJSON(url, function (data){
-        $.each(data, function(key, entry){
-            if(this.Name = currentWeapon){
-                if(currentLevel == 0){
-                    cDPShot = this.L0Damage;
-                    cRoF = this.L0RoF;
-                } 
-                else if(currentLevel == 10){
-                    cDPShot = this.L10Damage;
-                    cRoF = this.L10RoF;
-                }
-                document.getElementById('Shots').innerHTML = this.Shots;
-                
-                document.getElementById('DPShot').innerHTML = cDPShot;
-                document.getElementById('RoF').innerHTML = cRoF;
-                document.getElementById('DPS').innerHTML = cDPShot * this.Shots * cRoF;
-            }
-        })
-    })
+    if(currentLevel == 0){
+        cDPShot = weaponData[currentWeapon].L0Damage;
+        cRoF = weaponData[currentWeapon].L0RoF;
+    } 
+    else if(currentLevel == 10){
+        cDPShot = weaponData[currentWeapon].L10Damage;
+        cRoF = weaponData[currentWeapon].L10RoF;
+    }
+
+    document.getElementById('Shots').innerHTML = weaponData[currentWeapon].Shots;              
+    document.getElementById('DPShot').innerHTML = cDPShot;
+    document.getElementById('RoF').innerHTML = cRoF;
+    document.getElementById('DPS').innerHTML = cDPShot * weaponData[currentWeapon].Shots * cRoF;
 });
 
 
