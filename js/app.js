3efdc073921097d2f638083e14b17a8e6930c7fe
mossumossu <mosstanks@gmail.com>
2019-01-18 00:59:30 -0500

    rounding

--- js/app.js
+++ js/app.js
@@ -44,12 +44,14 @@ $(".dd-weapon").change(function () {
 
     cDPShot = weaponData[currentWeapon].L0Damage + ((weaponData[currentWeapon].L10Damage - weaponData[currentWeapon].L0Damage) / 10 ) * currentLevel;
     cRoF = weaponData[currentWeapon].L0RoF + ((weaponData[currentWeapon].L10RoF - weaponData[currentWeapon].L0RoF) / 10 ) * currentLevel;
+    cDPShot = Math.round(cDPShot);
+
     cDPS = cDPShot * cRoF * shots;
 
     document.getElementById('Shots').innerHTML = weaponData[currentWeapon].Shots;              
-    document.getElementById('DPShot').innerHTML = cDPShot;
-    document.getElementById('RoF').innerHTML = cRoF;
-    document.getElementById('DPS').innerHTML = cDPS;
+    document.getElementById('DPShot').innerHTML = Math.round(cDPShot);
+    document.getElementById('RoF').innerHTML = Number(cRoF).toFixed(2);
+    document.getElementById('DPS').innerHTML = Number(cDPS).toFixed(2);
 
     if(weaponData[currentWeapon].Rarity == "Purple"){
         plates = purplePlates;
@@ -64,15 +66,17 @@ $(".dd-weapon").change(function () {
     if(currentLevel < 10) {
         uDPShot = weaponData[currentWeapon].L0Damage + ((weaponData[currentWeapon].L10Damage - weaponData[currentWeapon].L0Damage) / 10 ) * nextLevel;
         uRoF = weaponData[currentWeapon].L0RoF + ((weaponData[currentWeapon].L10RoF - weaponData[currentWeapon].L0RoF) / 10 ) * nextLevel;
+        uDPShot = Math.round(uDPShot);
+
         uDPS = uDPShot * uRoF * shots;
         
         document.getElementById('uShots').innerHTML = weaponData[currentWeapon].Shots;              
         document.getElementById('uDPShot').innerHTML = uDPShot;
-        document.getElementById('uRoF').innerHTML = uRoF;
-        document.getElementById('uDPS').innerHTML = uDPS;
+        document.getElementById('uRoF').innerHTML = Number(uRoF).toFixed(2);
+        document.getElementById('uDPS').innerHTML = Number(uDPS).toFixed(2);
 
         document.getElementById('Plates').innerHTML = plates[currentLevel];              
-        document.getElementById('DPSPPlate').innerHTML = (uDPS - cDPS) / plates[currentLevel];
+        document.getElementById('DPSPPlate').innerHTML = Number((uDPS - cDPS) / plates[currentLevel]).toFixed(2);
     }
     else {
         document.getElementById('uShots').innerHTML = "-";              
